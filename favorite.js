66a28b471933a21c54245be2566ca95ee25af033
Kuan Ting Lin <kusakawazeusu@gmail.com>
2020-07-14 14:53:21 +0800

    錄影版本 version 1

--- favorite.js
+++ favorite.js
@@ -1,75 +1,66 @@
-(function () {
-  const BASE_URL = 'https://movie-list.alphacamp.io'
-  const INDEX_URL = BASE_URL + '/api/v1/movies/'
-  const POSTER_URL = BASE_URL + '/posters/'
-  const dataPanel = document.getElementById('data-panel')
-  const data = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
 
-  displayDataList(data)
+const movies = JSON.parse(localStorage.getItem('favoriteMovies'))
 
-  dataPanel.addEventListener('click', (event) => {
-    if (event.target.matches('.btn-show-movie')) {
-      showMovie(event.target.dataset.id)
-    } else if (event.target.matches('.btn-remove-favorite')) {
-      removeFavoriteItem(event.target.dataset.id)
-    }
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+
+function renderMovieList(data) {
+  let rawHTML = ''
+
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${
+          POSTER_URL + item.image
+        }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${
+            item.id
+          }">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${
+            item.id
+          }">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
   })
 
-  function displayDataList (data) {
-    let htmlContent = ''
-    data.forEach(function (item, index) {
-      htmlContent += `
-        <div class="col-sm-3">
-          <div class="card mb-2">
-            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
-            <div class="card-body movie-item-body">
-              <h6 class="card-title">${item.title}</h5>
-            </div>
-            <div class="card-footer">
-              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
-              <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">X</button>
-            </div>
-          </div>
-        </div>
-      `
-    })
-    dataPanel.innerHTML = htmlContent
+  dataPanel.innerHTML = rawHTML
+}
+
+function showMovieModal(id) {
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
+
+  axios.get(INDEX_URL + id).then((response) => {
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${
+      POSTER_URL + data.image
+    }" alt="movie-poster" class="img-fluid">`
+  })
+}
+
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
   }
+})
 
-  function showMovie (id) {
-    // get elements
-    const modalTitle = document.getElementById('show-movie-title')
-    const modalImage = document.getElementById('show-movie-image')
-    const modalDate = document.getElementById('show-movie-date')
-    const modalDescription = document.getElementById('show-movie-description')
-
-    // set request url
-    const url = INDEX_URL + id
-    console.log(url)
-
-    // send request to show api
-    axios.get(url).then(response => {
-      const data = response.data.results
-      console.log(data)
-
-      // insert data into modal ui
-      modalTitle.textContent = data.title
-      modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
-      modalDate.textContent = `release at : ${data.release_date}`
-      modalDescription.textContent = `${data.description}`
-    })
-  }
-
-  function removeFavoriteItem (id) {
-    // find movie by id
-    const index = data.findIndex(item => item.id === Number(id))
-    if (index === -1) return
-
-    // remove movie and update localStorage
-    data.splice(index, 1)
-    localStorage.setItem('favoriteMovies', JSON.stringify(data))
-
-    // repaint dataList
-    displayDataList(data)
-  }
-})()
+renderMovieList(movies)
--- index.js
+++ index.js
@@ -1,135 +1,125 @@
-const BASE_URL = "https://movie-list.alphacamp.io";
-const INDEX_URL = BASE_URL + "/api/v1/movies/";
-const POSTER_URL = BASE_URL + "/posters/";
+const BASE_URL = 'https://movie-list.alphacamp.io'
+const INDEX_URL = BASE_URL + '/api/v1/movies/'
+const POSTER_URL = BASE_URL + '/posters/'
+const MOVIES_PER_PAGE = 12
 
-const dataPanel = document.querySelector("#data-panel");
-const searchForm = document.querySelector("#search");
-const searchInput = document.querySelector("#search-input");
-const paginator = document.querySelector("#paginator");
-const ITEM_PER_PAGE = 12;
+const movies = []
+let filteredMovies = []
 
-const movies = [];
-let filteredMovies = [];
+const dataPanel = document.querySelector('#data-panel')
+const searchForm = document.querySelector('#search-form')
+const searchInput = document.querySelector('#search-input')
+const paginator = document.querySelector('#paginator')
 
-// listen to data panel
-dataPanel.addEventListener("click", function onPanelClicked(event) {
-  if (event.target.matches(".btn-show-movie")) {
-    showMovieModal(event.target.dataset.id);
-  } else if (event.target.matches(".btn-add-favorite")) {
-    console.log("Add favorite movie id: ", event.target.dataset.id);
-    addFavoriteItem(event.target.dataset.id);
-  }
-});
+function renderMovieList(data) {
+  let rawHTML = ''
 
-// listen to search form submit event
-searchForm.addEventListener("submit", function onSearchSubmitted(event) {
-  event.preventDefault();
-  let keyword = searchInput.value.toLowerCase();
-  if (!keyword) {
-    return alert("Please enter a valid keyword");
-  }
+  data.forEach((item) => {
+    // title, image
+    rawHTML += `<div class="col-sm-3">
+    <div class="mb-2">
+      <div class="card">
+        <img src="${
+          POSTER_URL + item.image
+        }" class="card-img-top" alt="Movie Poster">
+        <div class="card-body">
+          <h5 class="card-title">${item.title}</h5>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#movie-modal" data-id="${
+            item.id
+          }">More</button>
+          <button class="btn btn-info btn-add-favorite" data-id="${
+            item.id
+          }">+</button>
+        </div>
+      </div>
+    </div>
+  </div>`
+  })
 
-  filteredMovies = movies.filter((movie) =>
-    movie.title.toLowerCase().includes(keyword)
-  );
-
-  // 如果搜尋不到任何電影
-  if (filteredMovies.length === 0) {
-    alert("Cannot find any movies with keyword: " + keyword);
-    searchInput.value = "";
-  }
-
-  renderPaginator(filteredMovies.length);
-  renderMovieList(getMoviesByPage(1));
-});
-
-// listen to pagination click event
-paginator.addEventListener("click", function onPaginatorClicked(event) {
-  if (event.target.tagName !== "A") return;
-
-  console.log("Paginator clicked, page number: ", event.target.dataset.page);
-  renderMovieList(getMoviesByPage(event.target.dataset.page));
-});
-
-function getMoviesByPage(pageNum) {
-  const data = filteredMovies.length ? filteredMovies : movies;
-  let offset = (pageNum - 1) * ITEM_PER_PAGE;
-  return data.slice(offset, offset + ITEM_PER_PAGE);
+  dataPanel.innerHTML = rawHTML
 }
 
 function renderPaginator(amount) {
-  let totalPages = Math.ceil(amount / ITEM_PER_PAGE) || 1;
-  let pageItemContent = "";
-  for (let i = 0; i < totalPages; i++) {
-    pageItemContent += `
-        <li class="page-item">
-          <a class="page-link" href="javascript:;" data-page="${i + 1}">${
-      i + 1
-    }</a>
-        </li>
-      `;
+  const numberOfPages = Math.ceil(amount / MOVIES_PER_PAGE)
+  let rawHTML = ''
+
+  for (let page = 1; page <= numberOfPages; page++) {
+    rawHTML += `<li class="page-item"><a class="page-link" href="#" data-page="${page}">${page}</a></li>`
   }
-  paginator.innerHTML = pageItemContent;
+
+  paginator.innerHTML = rawHTML
 }
 
-function renderMovieList(data) {
-  let htmlContent = "";
-  data.forEach(function (item) {
-    htmlContent += `
-        <div class="col-sm-3">
-          <div class="card mb-2">
-            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
-            <div class="card-body movie-item-body">
-              <h5 class="card-title">${item.title}</h5>
-            </div>
-
-            <!-- "More" button -->
-            <div class="card-footer">
-              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
-              <!-- favorite button --> 
-              <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
-            </div>
-          </div>
-        </div>
-      `;
-  });
-  dataPanel.innerHTML = htmlContent;
+function getMoviesByPage(page) {
+  const data = filteredMovies.length ? filteredMovies : movies
+  const startIndex = (page - 1) * MOVIES_PER_PAGE
+  return data.slice(startIndex, startIndex + MOVIES_PER_PAGE)
 }
 
 function showMovieModal(id) {
-  // get elements
-  const modalTitle = document.getElementById("show-movie-title");
-  const modalImage = document.getElementById("show-movie-image");
-  const modalDate = document.getElementById("show-movie-date");
-  const modalDescription = document.getElementById("show-movie-description");
+  const modalTitle = document.querySelector('#movie-modal-title')
+  const modalImage = document.querySelector('#movie-modal-image')
+  const modalDate = document.querySelector('#movie-modal-date')
+  const modalDescription = document.querySelector('#movie-modal-description')
 
-  // send request to show api
   axios.get(INDEX_URL + id).then((response) => {
-    const data = response.data.results;
-
-    // insert data into modal ui
-    modalTitle.textContent = data.title;
-    modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`;
-    modalDate.textContent = `release at : ${data.release_date}`;
-    modalDescription.textContent = `${data.description}`;
-  });
+    const data = response.data.results
+    modalTitle.innerText = data.title
+    modalDate.innerText = 'Release date: ' + data.release_date
+    modalDescription.innerText = data.description
+    modalImage.innerHTML = `<img src="${
+      POSTER_URL + data.image
+    }" alt="movie-poster" class="img-fluid">`
+  })
 }
 
-function addFavoriteItem(id) {
-  const list = JSON.parse(localStorage.getItem("favoriteMovies")) || [];
-  const movie = movies.find((item) => item.id === Number(id));
+function addToFavorite(id) {
+  const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const movie = movies.find((movie) => movie.id === id)
 
-  if (list.some((item) => item.id === Number(id))) {
-    return alert(`${movie.title} is already in your favorite list.`);
+  if (list.some((movie) => movie.id === id)) {
+    return alert('此電影已經在收藏清單中！')
   }
 
-  list.push(movie);
-  alert(`Added ${movie.title} to your favorite list!`);
-  localStorage.setItem("favoriteMovies", JSON.stringify(list));
+  list.push(movie)
+  localStorage.setItem('favoriteMovies', JSON.stringify(list))
 }
 
+dataPanel.addEventListener('click', function onPanelClicked(event) {
+  if (event.target.matches('.btn-show-movie')) {
+    showMovieModal(Number(event.target.dataset.id))
+  } else if (event.target.matches('.btn-add-favorite')) {
+    addToFavorite(Number(event.target.dataset.id))
+  }
+})
+
+paginator.addEventListener('click', function onPaginatorClicked(event) {
+  if (event.target.tagName !== 'A') return
+
+  const page = Number(event.target.dataset.page)
+  renderMovieList(getMoviesByPage(page))
+})
+
+searchForm.addEventListener('submit', function onSearchFormSubmitted(event) {
+  event.preventDefault()
+  const keyword = searchInput.value.trim().toLowerCase()
+
+  filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(keyword)
+  )
+
+  if (filteredMovies.length === 0) {
+    return alert('Cannot find movies with keyword: ' + keyword)
+  }
+
+  renderPaginator(filteredMovies.length)
+  renderMovieList(getMoviesByPage(1))
+})
+
 axios.get(INDEX_URL).then((response) => {
-  movies.push(...response.data.results);
-  renderPaginator(movies.length);
-  renderMovieList(getMoviesByPage(1));
-});
+  movies.push(...response.data.results)
+  renderPaginator(movies.length)
+  renderMovieList(getMoviesByPage(1))
+})
